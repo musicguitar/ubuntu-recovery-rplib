b9211098306e8e8f79aa8a23ac4b638f06effa8c
Chen-Han Hsiao (Stanley) <stanley.hsiao@canonical.com>
2016-08-03 17:54:54 +0800

    add recovery const

---
+++ recovery.go
@@ -0,0 +1,7 @@
+package rplib
+
+const FACTORY_INSTALL = "factory_install"
+const FACTORY_RESTORE = "factory_restore"
+
+const BOOT_ENTRY_RECOVERY = "factory_restore"
+const BOOT_ENTRY_SNAPPY = "snappy_ubuntu_core"
